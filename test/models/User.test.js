2d288b9856ee6beafe8a375cfd7321d9081b6ac8
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-04-29 21:34:37 -0500

    Agregando clase User y su respectiva prueba

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,14 @@
-describe("Prueba de jest", () => {
-    test("Prueba 1", () => {
-        expect(10).toBe(0)
-    });
+const User = require('./../../app/models/User')
+
+describe("Tests para User", () => {
+    test("Creación de User", () => {
+        const user = new User(1, "mauriicioreyes", "Mauricio", "Bio", "dateCreated", "lastUpdated")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("mauriicioreyes")
+    })
 })
+// describe("Prueba de jest", () => {
+//     test("Prueba 1", () => {
+//         expect(10).toBe(0)
+//     });
+// })
