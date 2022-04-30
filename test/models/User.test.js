e25b7d738003bae74693e52908301e1a749ea6c8
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-04-29 21:45:18 -0500

    Agregando requerimiento 2 (modificación y respectiva prueba)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,10 +1,17 @@
 const User = require('./../../app/models/User')
 
 describe("Tests para User", () => {
-    test("Creación de User", () => {
+    test("Requerimiento 1: Creación de User", () => {
         const user = new User(1, "mauriicioreyes", "Mauricio", "Bio", "dateCreated", "lastUpdated")
         expect(user.id).toBe(1)
         expect(user.username).toBe("mauriicioreyes")
+    });
+
+    test("Requerimiento 2: Fechas en atributos de User", () => {
+        const user = new User(1, "carlosgarcia", "carlos", "bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+
     })
 })
 // describe("Prueba de jest", () => {
